ea11ad293f8fe639271a6cab433c076a51c001dc
Abdullah149081 <masudmd425@gmail.com>
2023-05-24 19:24:14 +0600

    header done

---
+++ src/Page/Shared/Header/Header.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+import React from "react";
+import ActiveLink from "./ActiveLink";
+
+const Header = () => {
+  const navbar = (
+    <>
+      <li>
+        <ActiveLink to="/">Home</ActiveLink>
+      </li>
+      <li>
+        <ActiveLink to="/contact">Contact us</ActiveLink>
+      </li>
+      <li>
+        <ActiveLink to="/dashboard">Dashboard</ActiveLink>
+      </li>
+      <li>
+        <ActiveLink to="/menu">Our menu</ActiveLink>
+      </li>
+      <li>
+        <ActiveLink to="/shop">Our shop</ActiveLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="bg-[#15151580]">
+      <div className="lg:px-12 py-4">
+        <div className="navbar justify-between">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+              </label>
+              <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 boss-navbar">
+                {navbar}
+              </ul>
+            </div>
+            <div className="uppercase text-xl flex flex-col">
+              <p className="text-white font-bold tracking-wide ">BISTRO BOSS</p>
+              <p className="text-white tracking-wider ">Restaurant</p>
+            </div>
+          </div>
+          <div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1 boss-navbar">{navbar}</ul>
+            </div>
+            <div className="navbar-end">
+              <a className="btn">Get started</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Page/Shared/Footer/Footer";
+import Header from "../Page/Shared/Header/Header";
 
 const Main = () => {
   return (
     <div>
-      <div>
+      <Header />
+      <div className="min-h-[calc(100vh-550px)]">
         <Outlet />
       </div>
       <Footer />
