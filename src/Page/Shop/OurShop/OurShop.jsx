ff4ce617ae5b897b265e5ab1350f683584d88088
Abdullah149081 <masudmd425@gmail.com>
2023-05-27 13:07:39 +0600

    category name change

--- src/Page/Shop/OurShop/OurShop.jsx
+++ src/Page/Shop/OurShop/OurShop.jsx
@@ -3,7 +3,7 @@ import MenuBanner from "../../../Components/MenuBanner/MenuBanner";
 import banner2 from "../../../assets/shop/banner2.jpg";
 
 const OurShop = () => {
-  const [activeTab, setActiveTab] = useState("Salad");
+  const [activeTab, setActiveTab] = useState("salad");
   const handleSelectTab = (tabName) => {
     setActiveTab(tabName);
   };
@@ -12,17 +12,17 @@ const OurShop = () => {
       <MenuBanner img={banner2} title="OUR SHOP" subTitle="Would you like to try a dish?" />
       <div className="boss-container">
         <div className="tabs  justify-center">
-          <button onClick={() => handleSelectTab("Salad")} className={`tab  ${activeTab === "Salad" ? "tab-active" : "tab-default"}`} type="button">
+          <button onClick={() => handleSelectTab("salad")} className={`tab  ${activeTab === "salad" ? "tab-active" : "tab-default"}`} type="button">
             Salad
           </button>
           <button onClick={() => handleSelectTab("pizza")} className={`tab  ${activeTab === "pizza" ? "tab-active" : "tab-default"}`} type="button">
             pizza
           </button>
-          <button onClick={() => handleSelectTab("soups")} className={`tab  ${activeTab === "soups" ? "tab-active" : "tab-default"}`} type="button">
+          <button onClick={() => handleSelectTab("soup")} className={`tab  ${activeTab === "soup" ? "tab-active" : "tab-default"}`} type="button">
             soups
           </button>
           <button onClick={() => handleSelectTab("desserts")} className={`tab  ${activeTab === "desserts" ? "tab-active" : "tab-default"}`} type="button">
-            soups
+            dessert
           </button>
           <button onClick={() => handleSelectTab("drinks")} className={`tab  ${activeTab === "drinks" ? "tab-active" : "tab-default"}`} type="button">
             drinks
