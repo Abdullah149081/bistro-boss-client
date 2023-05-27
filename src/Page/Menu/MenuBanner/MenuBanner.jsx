6d853985dbf6b65c546f8c307f9008b6eafc48c3
Abdullah149081 <masudmd425@gmail.com>
2023-05-27 10:40:45 +0600

    parallax done

--- src/Page/Menu/MenuBanner/MenuBanner.jsx
+++ src/Page/Menu/MenuBanner/MenuBanner.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { Parallax } from "react-parallax";
 
 const MenuBanner = ({ img, title, subTitle }) => {
   return (
-    <div className="bg-fixed" style={{ backgroundImage: `url("${img}")` }}>
-      <div className="boss-container  ">
-        <div className="flex lg:min-h-[800px] justify-center items-center">
-          <div className="relative min-h-[450px] w-full flex items-center justify-center">
-            <div className="absolute -z-0 bg-black inset-0 bg-opacity-50" />
-            <div className="relative">
-              <h1 className="text-4xl lg:text-[88px] text-white font-cinzel font-semibold">{title}</h1>
-              <p className="text-center text-2xl mt-7 text-white font-cinzel">{subTitle}</p>
+    <Parallax bgImageStyle={{ height: "100vh", width: "100%" }} blur={{ min: -50, max: 50 }} bgImage={img} bgImageAlt="the menu" strength={-200}>
+      <div>
+        <div className="boss-container">
+          <div className="flex lg:min-h-[600px] justify-center items-center">
+            <div className="relative lg:min-h-[450px] w-full flex items-center justify-center">
+              <div className="absolute -z-0 bg-black inset-0 bg-opacity-50" />
+              <div className="relative">
+                <h1 className="text-4xl lg:text-[88px] text-white font-cinzel font-semibold">{title}</h1>
+                <p className="text-center text-2xl mt-7 text-white font-cinzel">{subTitle}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
