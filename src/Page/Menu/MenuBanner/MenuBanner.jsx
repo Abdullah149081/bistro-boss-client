c59e97c0d868ac25f81f5e3ff8fb4fda6b54d2f6
Abdullah149081 <masudmd425@gmail.com>
2023-05-27 10:49:02 +0600

    fix responsive

--- src/Page/Menu/MenuBanner/MenuBanner.jsx
+++ src/Page/Menu/MenuBanner/MenuBanner.jsx
@@ -3,15 +3,15 @@ import { Parallax } from "react-parallax";
 
 const MenuBanner = ({ img, title, subTitle }) => {
   return (
-    <Parallax bgImageStyle={{ height: "100vh", width: "100%" }} blur={{ min: -50, max: 50 }} bgImage={img} bgImageAlt="the menu" strength={-200}>
+    <Parallax bgImageStyle={{ height: "100%", minHeight: "1240px", width: "100%" }} blur={{ min: -50, max: 50 }} bgImage={img} bgImageAlt="the menu" strength={-200}>
       <div>
         <div className="boss-container">
-          <div className="flex lg:min-h-[600px] justify-center items-center">
-            <div className="relative lg:min-h-[450px] w-full flex items-center justify-center">
+          <div className="flex lg:min-h-[700px] justify-center items-center">
+            <div className="relative py-6 lg:py-0 lg:min-h-[450px] w-full flex items-center justify-center">
               <div className="absolute -z-0 bg-black inset-0 bg-opacity-50" />
               <div className="relative">
-                <h1 className="text-4xl lg:text-[88px] text-white font-cinzel font-semibold">{title}</h1>
-                <p className="text-center text-2xl mt-7 text-white font-cinzel">{subTitle}</p>
+                <h1 className="text-2xl text-center lg:text-[88px] text-white font-cinzel font-semibold">{title}</h1>
+                <p className="text-center lg:text-2xl mt-4 lg:mt-12 text-white font-cinzel">{subTitle}</p>
               </div>
             </div>
           </div>
