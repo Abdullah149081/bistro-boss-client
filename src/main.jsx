7607b623ff976714a78bc08a41509fab27aa5903
Abdullah149081 <masudmd425@gmail.com>
2023-05-28 20:09:29 +0600

    firebae add

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Router";
 import "./index.css";
+import AuthProviders from "./providers/AuthProviders";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <AuthProviders>
+        <RouterProvider router={router} />
+      </AuthProviders>
     </HelmetProvider>
   </React.StrictMode>
 );
