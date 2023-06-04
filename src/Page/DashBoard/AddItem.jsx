33e993aa047271ee18dbd11010b1759c4680a3a7
Abdullah149081 <masudmd425@gmail.com>
2023-06-04 18:49:20 +0600

    add items page add

--- src/Page/DashBoard/AddItem.jsx
+++ src/Page/DashBoard/AddItem.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import PageTitle from "../../Components/PageTitle/PageTitle";
+import Title from "../../Components/SectionTitle/Title";
+
 const AddItem = () => {
   return (
-    <div>
-      <h1>add item</h1>
+    <div className="w-full">
+      <PageTitle title="Add Item" />
+      <div className="boss-container">
+        <Title>
+          <span className="text-white lg:text-[#D99904]">---What's new?---</span>
+          <span className="text-2xl lg:text-4xl">ADD AN ITEM</span>
+        </Title>
+        <div>
+          <div className="bg-[#F3F3F3] border p-4 lg:p-[50px]  ">
+            <h1>h</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
